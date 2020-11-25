ce1cbd6ae30145a5a552df452e03cc57f34ba655
McLP <wdv50jkc@gmail.com>
2020-11-25 14:38:05 +0100

    added measurements against running forever, especially if all buttons are already there

--- src/content_script.js
+++ src/content_script.js
@@ -1,25 +1,28 @@
 var tryAddButtons = setInterval(addButtons, 100);
+setTimeout(() => clearTimeout(tryAddButtons), 300000);
 function addButtons() {
-    for (let group of document.getElementsByClassName("btn-group-vertical")) if (group.childElementCount === 1) {
-        let playLink = new URL(group.firstElementChild.href);
-        let url = new URL(window.location.href);
-        let vars = {
-            ref_id : playLink.searchParams.get("ref_id"),
-            eid : playLink.searchParams.get("eid"),
-            event_id : playLink.searchParams.get("eid"),
-            cmd : "download",
-            cmdClass : "xocteventgui",
-            cmdNode : url.searchParams.get("cmdNode"),
-            baseClass : "ilObjPluginDispatchGUI"
+    for (let group of document.getElementsByClassName("btn-group-vertical")) {
+        if (group.childElementCount === 1) {
+            let playLink = new URL(group.firstElementChild.href);
+            let url = new URL(window.location.href);
+            let vars = {
+                ref_id : playLink.searchParams.get("ref_id"),
+                eid : playLink.searchParams.get("eid"),
+                event_id : playLink.searchParams.get("eid"),
+                cmd : "download",
+                cmdClass : "xocteventgui",
+                cmdNode : url.searchParams.get("cmdNode"),
+                baseClass : "ilObjPluginDispatchGUI"
+            }
+            let searchString = new URLSearchParams(vars).toString();
+            let button = document.createElement("a");
+            button.classList.add("btn");
+            button.classList.add("btn-info");
+            button.href=`ilias.php?${searchString}`;
+            button.innerHTML="Download";
+            button.target = "_self";
+            group.appendChild(button);
         }
-        let searchString = new URLSearchParams(vars).toString();
-        let button = document.createElement("a");
-        button.classList.add("btn");
-        button.classList.add("btn-info");
-        button.href=`ilias.php?${searchString}`;
-        button.innerHTML="Download";
-        button.target = "_self";
-        group.appendChild(button);
         clearTimeout(tryAddButtons);
     }
 }
